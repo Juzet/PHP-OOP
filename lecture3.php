33e410345e26dce2394aa556f7f53e8367b97bf1
Juzet Arana <arana.juzet@gmail.com>
2015-01-08 20:43:11 -0800

    hopefully im right

---
+++ lecture3.php
@@ -0,0 +1,35 @@
+<?php
+$hi = false;
+$bye = true;
+if (is_bool($hi) === true) {
+	echo "Yes, this is a boolean";
+}
+else {
+	echo "No, this is not a boolean";
+}
+$new = array('please' , 'do' , 'work');
+
+echo is_array($new) ? 'Ijn' : 'is not an Array';
+// echo "\n";</br>
+
+$old = 'blahs'; 
+
+echo is_array($old) ? 'I' : 'mean I guess';
+
+function get_me($oj)
+{
+    if (!is_object($oj)) {
+        return false;
+    }
+
+    return $oj->me;
+}
+
+// Declare a new class instance and fill up 
+// some values
+$oj = new stdClass();
+$oj->me = array('Juzet', 'Ron', 'Fen');
+
+var_dump(get_me(null));
+var_dump(get_me($oj));
+?>
